7d8811c1162ca1c8f7e638a59ddb15f19a5d1be9
Artek88 <artem.e3k@gmail.com>
2022-05-02 20:55:58 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 34;
+        int cash = 1000;
+        int newBalance = 1034;
+
+
+        boolean pay = true;
+
+        int bonus = 1000 / 100;
+                  //не могу понять как мне это всё вычислить
+
+        System.out.println("Бонус");
+    }
+
+}
